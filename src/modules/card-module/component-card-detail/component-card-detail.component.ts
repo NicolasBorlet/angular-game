e1e0a9f45268a9726f13f08b64cd56f98af2f700
KonyEnBikini <nicolas.borlet7@gmail.com>
2023-03-15 12:06:08 +0100

    work

--- src/modules/card-module/component-card-detail/component-card-detail.component.ts
+++ src/modules/card-module/component-card-detail/component-card-detail.component.ts
@@ -1,25 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 interface Card {
-  id: string;
-  name: string;
-  value: string;
+id: string;
+name: string;
+value: string;
 }
+
 @Component({
-  selector: 'app-component-card-detail',
-  templateUrl: './component-card-detail.component.html',
-  styleUrls: ['./component-card-detail.component.scss']
+selector: 'app-component-card-detail',
+templateUrl: './component-card-detail.component.html',
+styleUrls: ['./component-card-detail.component.scss']
 })
 export class ComponentCardDetailComponent implements OnInit {
-  @Input()
-  selectedCard!: Card;
+data: any = {};
+selectedCardId: string = '';
+selectedCard: Card | undefined;
 
-  constructor() {}
+constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
-  cardSelectedSubscription: any;
-  componentCardList: any;
-
-  ngOnInit() {
-
-  }
+ngOnInit() {
+  this.route.paramMap.subscribe(params => {
+  this.selectedCardId = params.get('id')!;
+  this.http.get<Card>(`http://localhost:3000/api/cards/${this.selectedCardId}`)
+  .subscribe(response => {
+    this.selectedCard = response;
+    console.log("Item", this.selectedCard);
+    });
+  });
+}
 }
--- src/modules/card-module/component-card-list/component-card-list.component.ts
+++ src/modules/card-module/component-card-list/component-card-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -17,7 +18,7 @@ export class ComponentCardListComponent implements OnInit {
   cards: any[] = [];
   selectedCard: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
 ngOnInit() {
   this.http.get<any[]>('http://localhost:3000/api/cards')
@@ -44,7 +45,7 @@ deleteCard(id: string) {
   });
 }
 
-  showDetails(card: any) {
-    this.selectedCard = card;
+  showDetails(id: string) {
+    this.router.navigateByUrl(`/card/${id}`);
   }
 }
